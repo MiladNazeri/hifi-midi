643c34454dd896b64e946e562aa4129a646089a5
milad <milad@highfidelity.io>
2018-01-16 19:55:08 -0800

    more libs

--- html/js/midi.js
+++ html/js/midi.js
@@ -9,6 +9,55 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
+
+/*
+var dial = Nexus.Add.Dial('#instrument',{
+  'size': [100,100]
+});
+
+var slider = Nexus.Add.Slider('#instrument',{
+  'size': [25,100]
+});
+
+// then, to remove them tlater
+dial.destroy();
+slider.destroy();
+
+var dial = new Nexus.Dial("#dial");
+
+dial.on('change',function(v) {
+  // v holds the new numeric value of the dial
+});
+
+
+var slider = new Nexus.Slider("#slider");
+
+slider.on('click',function() {
+  console.log('clicked!');
+});
+
+slider.on('release',function() {
+  console.log('released');
+});
+
+
+{
+  min: 0,
+  max: 10,
+  step: 1,
+  mode: 'absolute'
+}
+
+var dial = new Nexus.Dial('#target',{
+  'size': [75,75],
+  'interaction': 'radial', // "radial", "vertical", or "horizontal"
+  'mode': 'relative', // "absolute" or "relative"
+  'min': 0,
+  'max': 1,
+  'step': 0,
+  'value': 0
+})
+*/
 var isUsingToolbar = false,
     EVENT_BRIDGE_TYPE = "midi",
     BODY_LOADED_ACTION = "bodyLoaded",
@@ -20,6 +69,14 @@ var isUsingToolbar = false,
     elShowInfoButton,
     isDisplayingInstructions,
     elSpinner;
+
+var dialInputMin = new Nexus.Dial('#input-min-dial');
+var dialInputMax = new Nexus.Dial('#input-max-dial');
+var dialOutputMin = new Nexus.Dial('#output-min-dial');
+var dialOutputMax = new Nexus.Dial('#output-max-dial');
+var dialCurrent = new Nexus.Dial('#current-dial');
+
+
 
 function showInstructions() {
         isDisplayingInstructions = true;
